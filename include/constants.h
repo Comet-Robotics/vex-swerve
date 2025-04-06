e6b4ea06ca70a13ae64a2f8e2420405cf3f45a01
Keval Shah <kevals2005@gmail.com>
2025-04-06 02:47:39 -0500

    fixed up some C++-specific oddities using ChatGPT

--- include/constants.h
+++ include/constants.h
@@ -8,56 +8,56 @@ namespace constants {
 
         // for each of these, first num is the motor controlling the top gear, second is controlling the bottom gear
         // 1
-        inline constexpr std::array<int8_t, 2> FRONT_RIGHT_PORTS = {
+        constexpr std::array<int8_t, 2> FRONT_RIGHT_PORTS = {
             0,
             0
         };
         // 2
-        inline constexpr std::array<int8_t, 2> FRONT_LEFT_PORTS = {
+        constexpr std::array<int8_t, 2> FRONT_LEFT_PORTS = {
             0,
             0
         };
         // 3
-        inline constexpr std::array<int8_t, 2> BACK_LEFT_PORTS = {
+        constexpr std::array<int8_t, 2> BACK_LEFT_PORTS = {
             0,
             0
         };
         // 4
-        inline constexpr std::array<int8_t, 2> BACK_RIGHT_PORTS = {
+        constexpr std::array<int8_t, 2> BACK_RIGHT_PORTS = {
             0,
             0
         };
         
-        inline constexpr int8_t IMU_PORT = 0;
+        constexpr int8_t IMU_PORT = 0;
     }
 
     namespace drivetrain {
-        inline constexpr pros::MotorGears CHASSIS_INTERNAL_GEARSET = pros::MotorGears::blue;
+        constexpr pros::MotorGears CHASSIS_INTERNAL_GEARSET = pros::MotorGears::blue;
 
-        inline constexpr int WHEEL_DIAMETER = 0;
+        constexpr int WHEEL_DIAMETER = 0;
         
         inline pros::Imu IMU(ports::IMU_PORT);
 
-        inline constexpr double ROTATION_FACTOR = 1;
-        inline constexpr double TRACK_LENGTH = 0.0; // distance between front and back wheels
-        inline constexpr double TRACK_WIDTH = 0.0; // distance between left and right wheels
+        constexpr double ROTATION_FACTOR = 1;
+        constexpr double TRACK_LENGTH = 0.0; // distance between front and back wheels
+        constexpr double TRACK_WIDTH = 0.0; // distance between left and right wheels
 
-        inline constexpr std::array<double, 3> FRONT_LEFT_PID = {
+        constexpr std::array<double, 3> FRONT_LEFT_PID = {
             0.0, 
             0.0, 
             0.0
         };
-        inline constexpr std::array<double, 3> FRONT_RIGHT_PID = {
+        constexpr std::array<double, 3> FRONT_RIGHT_PID = {
             0.0, 
             0.0, 
             0.0
         };
-        inline constexpr std::array<double, 3> BACK_LEFT_PID = {
+        constexpr std::array<double, 3> BACK_LEFT_PID = {
             0.0, 
             0.0, 
             0.0
         };
-        inline constexpr std::array<double, 3> BACK_RIGHT_PID = {
+        constexpr std::array<double, 3> BACK_RIGHT_PID = {
             0.0, 
             0.0, 
             0.0
