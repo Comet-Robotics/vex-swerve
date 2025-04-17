9774e1be9b86c3e5f8a18d2739f7451a3382b2ae
Udunen <cole.huffine@gmail.com>
2025-04-16 20:49:39 -0500

    added odom pod class
    getting late, will make odometry class later

---
+++ include/motion/odometry/OdometryPod.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include <math.h>
+#include "pros/rotation.hpp"
+
+class OdometryPod {
+public:
+    OdometryPod(int8_t sensorPort, double wheelDiameter) :
+    sensor(sensorPort),
+    wheelRadius(wheelDiameter/2) {}
+
+    void tare() {
+        sensor.reset_position();
+    }
+
+    double getTraveledDistance() const {
+        return sensor.get_position()/100.0 * wheelRadius;
+    }
+
+private:
+    pros::Rotation sensor;
+    double wheelRadius;
+};
